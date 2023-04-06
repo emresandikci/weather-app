377de2f7b6709d6cc539e47da76e24b9055d9ce5
mr.san <me@emresandikci.com>
2023-04-06 08:07:24 +0300

    feat(container): add app layout

---
+++ src/containers/mainLayout/index.tsx
@@ -0,0 +1,27 @@
+import { useIsFetching } from '@tanstack/react-query';
+import { Loading } from 'components';
+import { useAppContext } from 'context/appContext';
+import { IBaseComponent } from 'models';
+
+import { usePhoto } from 'store';
+import { DEFAULT_BACKGROUND } from 'utils';
+
+export default function MainLayout({ children }: IBaseComponent) {
+  const isFetching = useIsFetching();
+  const { locationQuery } = useAppContext();
+
+  const { photoQueries } = usePhoto({ q: '' });
+  const { data: photo } = photoQueries;
+
+  return (
+    <div
+      className="relative bg-cover bg-center"
+      style={{
+        backgroundImage: `url('${photo ? photo?.hits[0].largeImageURL : DEFAULT_BACKGROUND}')`,
+      }}
+    >
+      {!!isFetching && <Loading className="mt-14" />}
+      <div className="flex min-h-screen flex-col bg-gray-700/60 backdrop-blur">{children}</div>
+    </div>
+  );
+}
